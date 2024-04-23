e0f46a29a54d4925e419c61a59dfed81442515dc
Atomjoy <atomjoy.official@gmail.com>
2024-04-23 14:08:32 +0200

    C# 2FA Code generator
    C# 2FA from json.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using OtpNet;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Item
+    {        
+        public string name = "";
+        public string code = "";
+        // public DateTime datetime;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Random hash
+            var key = KeyGeneration.GenerateRandomKey(20);
+            var base32String = Base32Encoding.ToString(key);
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Random {0}", base32String);
+            Console.WriteLine("-------------------------------------");
+
+            // using StreamReader r = new(@"D:\Git\file.json");
+            // From JSON
+            using StreamReader r = new(@"secrets\secrets.json");
+            string json = r.ReadToEnd();
+            if (json != null)
+            {
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);                
+                dynamic array = JsonConvert.DeserializeObject(json);
+                foreach (var item in array)
+                {
+                    string itemName = item.name;
+                    string itemCode = item.code;                    
+                    var otpCode = new Totp(Base32Encoding.ToBytes(itemCode)).ComputeTotp(DateTime.UtcNow);                    
+                    Console.WriteLine("{0} ==> {1}", itemName, otpCode);                    
+                    Console.WriteLine("-------------------------------------");
+                }
+            }
+
+            Console.WriteLine("\nPress any key to exit.");            
+            Console.ReadKey();
+        }
+    }
+}
