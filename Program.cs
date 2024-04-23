441539fa5513805c8c34559f41024d456e9a39f7
Atomjoy <atomjoy.official@gmail.com>
2024-04-23 15:07:27 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -28,25 +28,25 @@ namespace HelloWorld
             // using StreamReader r = new(@"D:\Git\file.json");
             // From JSON
             using StreamReader r = new(@"secrets\secrets.json");
-            string json = r.ReadToEnd();
+            string? json = r.ReadToEnd();
             if (json != null)
             {
-                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+                List<Item>? items = JsonConvert.DeserializeObject<List<Item>>(json);
 
                 while (true)
                 {
-                    dynamic array = JsonConvert.DeserializeObject(json);
+                    dynamic? array = JsonConvert.DeserializeObject(json) ?? Array.Empty<string>();
 
                     foreach (var item in array)
                     {
                         string itemName = item.name;
                         string itemCode = item.code;
-                        var otpCode = new Totp(Base32Encoding.ToBytes(itemCode)).ComputeTotp(DateTime.UtcNow);                        
+                        var otpCode = new Totp(Base32Encoding.ToBytes(itemCode)).ComputeTotp(DateTime.UtcNow);
                         Console.WriteLine("{0}", DateTime.UtcNow.ToString());
                         Console.WriteLine("===> {0}", itemName);
                         Console.WriteLine("======> {0}", otpCode);
                         Console.WriteLine("----------------------------------------");
-                    }
+                    }                 
 
                     Thread.Sleep(15000); // 30 second delay
                     Console.Clear();
